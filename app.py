91c40c4b1c8d95bb45dce4da8344c33a01007c56
KalinduBihan <klindubihanfdo@gmail.com>
2025-02-25 15:03:32 +0530

    commit 10

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import pickle
 import numpy as np
 import pandas as pd
 import os
+from flask_cors import CORS
 
 # Initialize Flask app
 app = Flask(__name__, template_folder="templates")
+CORS(app)
 
 # Load the trained model
 MODEL_PATH = "artifacts/model_stress.pickle"
@@ -66,14 +68,14 @@ def predict():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
-# from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify, render_template
 # import pickle
 # import numpy as np
 # import pandas as pd
 # import os
 
 # # Initialize Flask app
-# app = Flask(__name__)
+# app = Flask(__name__, template_folder="templates")
 
 # # Load the trained model
 # MODEL_PATH = "artifacts/model_stress.pickle"
@@ -87,30 +89,40 @@ if __name__ == "__main__":
 # def inference_stress(data):
 #     id_ = data.get("id", "Unknown")
 #     records = data.get("data", [])
-    
+
 #     if not records:
 #         return {"error": "No data provided"}
-    
+
 #     df = pd.DataFrame(records)
 #     if "TEMP" not in df or "HR" not in df:
 #         return {"error": "Missing required fields: HR, TEMP"}
-    
+
 #     TEMP = df["TEMP"].values
 #     if np.any((TEMP < 26) | (TEMP > 38)):
 #         return {"id": id_, "stress": "Temperature should be between 26 and 38"}
-    
+
 #     X = df.drop(columns=["datetime"], errors='ignore').values
 #     P = cls.predict(X)
 #     avg_stress = np.sum(P) / (len(P) * 2)
-    
+
 #     if avg_stress < 0.25:
 #         status = "Excellent"
 #     elif avg_stress < 0.75:
 #         status = "Adequate"
 #     else:
 #         status = "Needs Improvement"
-    
+
 #     return {"id": id_, "stress": status, "average_stress": f"{avg_stress * 100:.2f}%"}
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @app.route("/data", methods=["POST"]) 
+# def message():
+#     data = request.get_json()  # Get JSON data from request body
+#     print("Received Data:", data)  # Print received data to the console
+#     return jsonify({"message": "good", "received": data})  # Send response back
 
 # @app.route("/predict", methods=["POST"])
 # def predict():
